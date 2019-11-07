206c6dd5b66cfb2ebc615e8b014a97bccf00a43c
ritambasu61 <57460456+ritambasu61@users.noreply.github.com>
2019-11-07 15:35:41 +0530

    Initial code using list method

---
+++ Fibonacci.py
@@ -0,0 +1,13 @@
+"""
+created by: Ritam Basu
+created on:06/11/19
+created for: assignment1.
+
+"""
+
+from math import *
+s = [1,1]
+for i in range(0,8):
+    t = s[i] + s[i+1]
+    s.append(t)
+print (s)
