e3c80212bae4ff1cc7186fa87488a551c3b19089
ritambasu61 <57460456+ritambasu61@users.noreply.github.com>
2019-11-07 15:38:53 +0530

    Defining Fibonacci function(module)

---
+++ Fibonacci2.py
@@ -0,0 +1,15 @@
+"""
+created by: Ritam Basu
+created on:06/11/19
+created for: assignment1.
+
+"""
+from math import *
+def f(n):
+ s = [1,1]
+ for i in range(0,n-2):
+  t = s[i] + s[i+1]
+  s.append(t)
+ return s   
+    
+
